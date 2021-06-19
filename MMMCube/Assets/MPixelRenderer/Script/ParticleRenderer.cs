ace54668a993e8e3c22fc88b8796ffb511ae40c4
iamabigartist <1548016008@qq.com>
2021-06-19 16:58:11 +0800

    Part of the particle render

--- MMMCube/Assets/MPixelRenderer/Script/ParticleRenderer.cs
+++ MMMCube/Assets/MPixelRenderer/Script/ParticleRenderer.cs
@@ -1,44 +1,55 @@
-using System. Collections;
-using System. Collections. Generic;
+using System.Collections;
+using System.Collections.Generic;
 
 using UnityEngine;
 
+/// <summary>
+/// This renderer use a input <see cref="ComputeBuffer"/> and the PointCloudShader to render a point cloud
+/// </summary>
 public class ParticleRenderer : MonoBehaviour
 {
-    public Material material;
-    protected int number = 20000000;
-    protected ComputeBuffer compute_buffer;
+    private Material _cloud_render;
+    private ComputeBuffer _particle_buffer;
 
-    private struct Point
-    {
-        public Vector3 position;
-    }
+    #region Config
+
+    private Color _main_color;
+    private Color _outline_color;
+    private float _outline_size;
+
+    #endregion Config
 
     private void Start ()
     {
-        compute_buffer = new ComputeBuffer( number , sizeof( float ) * 3 , ComputeBufferType. Default );
-        Point [ ] cloud = new Point [ number ];
-        for ( uint i = 0; i < number; ++i )
-        {
-            cloud [ i ] = new Point();
-            cloud [ i ]. position = new Vector3();
-            cloud [ i ]. position. x = Random. Range( -200.0f , 200.0f );
-            cloud [ i ]. position. y = Random. Range( -200.0f , 200.0f );
-            cloud [ i ]. position. z = Random. Range( -200.0f , 200.0f );
-        }
-        compute_buffer. SetData( cloud );
+        _particle_buffer = null;
+        _cloud_render = new Material( Shader.Find( "Point Cloud" ) );
     }
 
-    private void OnPostRender ()
+    #region Interface
+
+    /// <summary>
+    /// </summary>
+    /// <param name="outline_size">The ouline paticle pos = outline_size * paticle pos</param>
+    public void SetData ( ComputeBuffer particle_buffer , Color main_color , Color outline_color , float outline_size )
     {
-        material. SetPass( 0 );
-        material. SetBuffer( "cloud" , compute_buffer );
-
-        Graphics. DrawProceduralNow( MeshTopology. Points , number , 1 );
+        _particle_buffer = particle_buffer;
+        _main_color = main_color;
+        _outline_color = outline_color;
+        _outline_size = outline_size;
     }
 
-    private void OnDestroy ()
+    #endregion Interface
+
+    #region Render
+
+    private void OnRenderObject ()
     {
-        compute_buffer. Release();
+        if ( _particle_buffer == null ) return;
+
+        _cloud_render.SetBuffer( "cloud" , _particle_buffer );
+        _cloud_render.SetPass( 0 );
+        Graphics.DrawProceduralNow( MeshTopology.Points , _particle_buffer.count );
     }
+
+    #endregion Render
 }
